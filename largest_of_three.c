04c42035d5e2de041f56ca788344696b56bf8489
Rishabh Vashisth Tiwari <tiwaririshabh026@gmail.com>
2025-08-31 11:58:51 +0530

    Add files via upload
    largest of three number

---
+++ largest_of_three.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+
+  int a,b,c;
+  printf("Enter numbers :");
+  scanf("%d,%d,%d",&a,&b,&c);
+
+  if(a>=b && a>=c){
+    printf("%d is largest number",a);
+
+  }else if(b>=a && b>=c){
+    printf("%d is largest number",b);
+
+  }else if(c>=a && c>=b){
+    printf("%d is largest number",c);
+  } 
+  
+  return 0;
+}
