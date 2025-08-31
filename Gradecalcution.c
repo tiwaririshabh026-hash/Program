fbd1e32b3fe49d17a3a14a1544b1554f2edc5d24
Rishabh Vashisth Tiwari <tiwaririshabh026@gmail.com>
2025-08-31 19:03:44 +0530

    Add files via upload

---
+++ Gradecalcution.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int marks;
+
+    printf("Enter your marks (0-100): ");
+    scanf("%d", &marks);
+
+    if (marks >= 90 && marks <= 100) {
+        printf("Grade: A\n");
+    }
+    else if (marks >= 80) {
+        printf("Grade: B\n");
+    }
+    else if (marks >= 70) {
+        printf("Grade: C\n");
+    }
+    else if (marks >= 60) {
+        printf("Grade: D\n");
+    }
+    else if (marks >= 50) {
+        printf("Grade: E\n");
+    }
+    else if (marks >= 0) {
+        printf("Grade: F (Fail)\n");
+    }
+    else {
+        printf("Invalid Marks! Please enter between 0-100.\n");
+    }
+
+    return 0;
+}
