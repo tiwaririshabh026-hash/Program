9d7748951f102a07f461cc99a8a01c96e672feca
Rishabh Vashisth Tiwari <tiwaririshabh026@gmail.com>
2025-08-31 19:52:32 +0530

    Add files via upload

---
+++ check_uppercase_lowercase.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+    
+    char ch;
+    printf("Enter a character:");
+    scanf("%c",&ch);
+
+    if(ch >='a' && ch <='z'){
+        printf("%c is Lowercase\n",ch);
+    }
+    else if(ch >= 'A' && ch <= 'Z'){
+        printf("%c is Uppercase\n",ch);
+    }
+    else{
+        printf("%c is not an alphabet\n",ch);
+    }
+    return 0;
+}
